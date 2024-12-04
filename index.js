5caa0eaf226d013b20957e7fcb7f7c94d96dd84a
fulldevnuts <fullnuts5@gmail.com>
2024-12-04 22:34:33 +0000

    implement a ping slash command

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require("discord.js");
 
 require("dotenv").config()
+const DISCORD_SERVER_ID = process.env.DISCORD_SERVER_ID;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+  const ping = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+
+  client.application.commands.create(ping, DISCORD_SERVER_ID);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+  console.log(interaction);
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    interaction.reply("Pong!");
+    return;
+  }
+
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
